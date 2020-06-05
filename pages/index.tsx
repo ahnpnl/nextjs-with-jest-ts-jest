7ccd681777bc7a5df0da6d363754a2966737371a
Ahn <anhpnnd@gmail.com>
2020-06-05 09:42:46 +0200

    update project to see failed test cases

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 import Head from 'next/head';
-import MyComponent from "../components/MyComponent";
 
 const Home = () => (<Fragment>
   <Head>
     <title>Next.js + Jest + TypeScript</title>
   </Head>
   <p>A simple example repo</p>
-  <MyComponent />
 </Fragment>);
 
 export default Home;
